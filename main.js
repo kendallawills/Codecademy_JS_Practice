f7a6e2334cd49a724b5db73693492878407faa44
Kendall <kendallawills@gmail.com>
2023-07-16 14:55:56 -0400

    How Old

--- main.js
+++ main.js
@@ -134,3 +134,23 @@ console.log(`You had ${numImaginaryFriends(10)} imaginary friends.`);
 const sillySentence = (adjective, verb, noun) => `I am so ${adjective} because I ${verb} coding! Time to write some more awesome ${noun}!`;
 
 console.log(sillySentence('excited', 'love', 'functions'));
+
+// How Old
+
+const howOld = (age, year) => {
+    let dateToday = new Date();
+    let thisYear = dateToday.getFullYear();
+
+    const yearDifference = year - thisYear;
+    const newAge = age + yearDifference;
+
+    if (newAge > age) {
+        return `You will be ${newAge} in the year ${year}.`;
+    } else if (newAge < 0) {
+        return `The year ${year} was ${-newAge} years before you were born.`;
+    } else {
+        return `You were ${newAge} in the year ${year}.`;
+    }
+}
+
+console.log(howOld(34, 1962));
