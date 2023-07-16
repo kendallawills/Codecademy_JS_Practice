ebaafd7a80f381ebd057235b2be38f0d8d27aa6e
Kendall <kendallawills@gmail.com>
2023-07-16 15:30:51 -0400

    Emoticon

--- main.js
+++ main.js
@@ -200,3 +200,24 @@ const tipCalculator = (quality, total) => {
 }
 
 console.log(tipCalculator('good', 100));
+
+// Emoticon
+
+const toEmoticon = meaning => {
+    switch (meaning) {
+        case 'shrug':
+            return '|_{"}_|';
+        case 'smiley face':
+            return ':)';
+        case 'frowny face':
+            return '):';
+        case 'winky face':
+            return ';)';
+        case 'heart':
+            return '<3';
+        default:
+            return '|_(* ~ *)_|';
+    }
+}
+
+console.log(toEmoticon('silly'));
